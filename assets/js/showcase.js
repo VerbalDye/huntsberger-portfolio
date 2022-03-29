76dd2385654e46d8cab8d8d7b4602e5211374489
Dustin Huntsberger <dhunts258@gmail.com>
2022-03-29 14:52:07 -0400

    it basically works, mobile design next

--- assets/js/showcase.js
+++ assets/js/showcase.js
@@ -21,8 +21,26 @@ var updateWidth = function () {
     });
 }
 
-var adjustShowcaseWidth = function (event) {
+var handleMouseOver = function(event) {
     var targetEl = event.target;
+    var bodyEl = document.querySelector("#" + targetEl.id + " .showcase-body");
+    var descriptionEl = document.querySelector("#" + targetEl.id + " .showcase-body p");
+
+    if (showcaseWidth < 900) {
+        descriptionEl.style.width = "100%";
+    } else if (showcaseWidth < 1250) {
+        descriptionEl.style.width = Math.floor(showcaseWidth / 2 - 21) + "px";
+    } else {
+        descriptionEl.style.width = Math.floor(showcaseWidth / 3 - 21) + "px";
+    };
+
+    bodyEl.style.display = 'flex';
+
+    adjustShowcaseWidth(targetEl);
+}
+
+var adjustShowcaseWidth = function (targetEl) {
+
     if (targetEl.className == "showcase-el" && showcaseWidth >= 900) {
         var row = targetEl.getAttribute("data-row");
         var totalWidthUsed = 0;
@@ -41,6 +59,6 @@ updateWidth();
 
 window.onresize = updateWidth;
 for(var i = 0; i < showcaseEl.childElementCount; i++) {
-    showcaseElList[i].addEventListener("mouseenter", adjustShowcaseWidth);
+    showcaseElList[i].addEventListener("mouseenter", handleMouseOver);
     showcaseElList[i].addEventListener("mouseleave", updateWidth);
 };
