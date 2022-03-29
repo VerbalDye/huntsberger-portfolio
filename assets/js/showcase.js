6a0b262b19c1a0d0a03e773bea22fb260ff0fe81
Dustin Huntsberger <dhunts258@gmail.com>
2022-03-29 16:20:42 -0400

    added missteps and obstacles

--- assets/js/showcase.js
+++ assets/js/showcase.js
@@ -8,6 +8,14 @@ var updateWidth = function () {
     var count = 0;
 
     showcaseElList.forEach(function (element) {
+        var bodyEl = document.querySelector("#" + element.id + " .showcase-body");
+        if(bodyEl.style.display == 'flex') {
+            bodyEl.style.opacity = '0';
+            setTimeout(function() {
+                bodyEl.style.display = 'none'
+            } , 1000);
+        }
+
         if (showcaseWidth < 900) {
             element.style.width = "100%";
         } else if (showcaseWidth < 1250) {
@@ -25,6 +33,13 @@ var handleMouseOver = function(event) {
     var targetEl = event.target;
     var bodyEl = document.querySelector("#" + targetEl.id + " .showcase-body");
     var descriptionEl = document.querySelector("#" + targetEl.id + " .showcase-body p");
+    bodyEl.style.display = 'flex';
+    bodyEl.style.opacity = '0';
+    
+    waitForElm("#" + targetEl.id + " .showcase-body").then((elm) => {
+        console.log('Element is ready');
+        elm.style.opacity = '1';
+    });
 
     if (showcaseWidth < 900) {
         descriptionEl.style.width = "100%";
@@ -34,7 +49,7 @@ var handleMouseOver = function(event) {
         descriptionEl.style.width = Math.floor(showcaseWidth / 3 - 21) + "px";
     };
 
-    bodyEl.style.display = 'flex';
+
 
     adjustShowcaseWidth(targetEl);
 }
@@ -55,10 +70,31 @@ var adjustShowcaseWidth = function (targetEl) {
     }
 }
 
+var waitForElm = function(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
 updateWidth();
 
 window.onresize = updateWidth;
 for(var i = 0; i < showcaseEl.childElementCount; i++) {
     showcaseElList[i].addEventListener("mouseenter", handleMouseOver);
+    showcaseElList[i].addEventListener("touchenter", handleMouseOver);
     showcaseElList[i].addEventListener("mouseleave", updateWidth);
 };
